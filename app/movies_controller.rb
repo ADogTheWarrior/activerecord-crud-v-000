3d5ca5725557d332faa6045a601e9f4160e9e7b4
Adam Fudge <coolfudge@gmail.com>
2017-11-14 21:12:58 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,12 @@ end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    __
+    binding.pry
+    m.title = "David"
+    m.release_date = "Code Artist"
+    m.director = 
+    m.lead = 
+    m.in_theaters = 
   end
 end
 
